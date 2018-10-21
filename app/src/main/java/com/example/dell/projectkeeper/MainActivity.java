87a0a5d04da9ad6e7a4d8afac5013ba82bc9e451
HimanshuKaushikHK <himanshukaushik016@gmail.com>
2018-10-21 23:21:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/dell/projectkeeper/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dell.projectkeeper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginbutton;
+    Intent int1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginbutton = (Button)findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int1 = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(int1);
+
+            }
+        });
+    }
+}
